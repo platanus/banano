dff819aeb828226f25b611540b4e42276412ebb5
Guillermo Moreno <gmq@guillermomoreno.cl>
2023-03-10 10:07:20 -0300

    chore(): update histoire

--- histoire.config.ts
+++ histoire.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'histoire';
+import { HstVue } from '@histoire/plugin-vue';
 
 export default defineConfig({
   setupFile: '/src/histoire.setup.ts',
+  plugins: [
+    // eslint-disable-next-line new-cap
+    HstVue(),
+  ],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
-/// <reference types="histoire" />
+/// <reference types="@histoire/plugin-vue/components" />
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types

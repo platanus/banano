a015c9bce80520d0570154a9db7c942123305d18
Diego Fernández <difernandez@uc.cl>
2023-05-18 12:31:20 -0400

    feat(bn-checkbox-styles): only apply ring when focus-visible

--- src/components/BnCheckbox/BnCheckbox.styles.cjs
+++ src/components/BnCheckbox/BnCheckbox.styles.cjs
@@ -11,10 +11,11 @@ module.exports = {
       },
     },
     '&__input': {
-      '@apply mr-2 rounded border-gray-300': {},
+      '@apply mr-2 rounded border-gray-300 focus:outline-none focus:ring-0 focus:ring-offset-0': {},
+      '@apply focus-visible:ring-offset-2 focus-visible:ring-2': {},
       colors: {
         '@apply text-varColor-600': {},
-        '@apply focus:outline-none focus:border-varColor-600 focus:ring-2 focus:ring-varColor-600': {},
+        '@apply focus-visible:border-varColor-600 focus-visible:ring-varColor-600': {},
       },
     },
   },

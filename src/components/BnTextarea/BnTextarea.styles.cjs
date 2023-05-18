8261459e84f533b6537d33613e55255f1c045678
Diego Fernández <difernandez@uc.cl>
2023-05-18 11:57:17 -0400

    feat(bn-textarea-styles): use banano-error color
    Also, use only ring, no outline

--- src/components/BnTextarea/BnTextarea.styles.cjs
+++ src/components/BnTextarea/BnTextarea.styles.cjs
@@ -2,13 +2,13 @@ module.exports = {
   '.bn-textarea': {
     '@apply w-full': {},
     '@apply py-3 px-3 rounded-lg border border-gray-200 text-banano-text-foreground bg-banano-bg': {},
-    '@apply placeholder:text-banano-text-muted': {},
+    '@apply placeholder:text-banano-text-muted focus:outline-none focus:ring': {},
     '@apply disabled:bg-gray-100 disabled:opacity-75 disabled:cursor-not-allowed': {},
     colors: {
-      '@apply focus:border-varColor-600 focus:ring-varColor-600': {},
+      '@apply focus:border-varColor-600 focus:ring-varColor-600/25': {},
     },
     '&--error': {
-      '@apply border border-red-300 text-red-700 ring-red-300 !important': {},
+      '@apply border border-banano-error focus:ring-banano-error/25 !important': {},
     },
   },
 };

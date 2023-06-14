781615c1cb0b78242d2f17be397599e36dbfbad1
Guillermo Moreno <gmq@guillermomoreno.cl>
2023-06-14 11:54:08 -0400

    feat(): export components on their own

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,4 +8,4 @@ import BnTextarea from './BnTextarea/BnTextarea.vue';
 import BnFileInput from './BnFileInput/BnFileInput.vue';
 import BnCheckbox from './BnCheckbox/BnCheckbox.vue';
 
-export default { Btn, BnInput, BnListbox, BnModal, BnToggle, BnPagination, BnTextarea, BnFileInput, BnCheckbox };
+export { Btn, BnInput, BnListbox, BnModal, BnToggle, BnPagination, BnTextarea, BnFileInput, BnCheckbox };

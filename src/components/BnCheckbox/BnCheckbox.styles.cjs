84970985efcdd399613257203cc90efbba17d9fd
Diego Fernández <difernandez@uc.cl>
2023-07-26 11:10:17 -0400

    feat(bn-checkbox): unnest styles

--- src/components/BnCheckbox/BnCheckbox.styles.cjs
+++ src/components/BnCheckbox/BnCheckbox.styles.cjs
@@ -6,9 +6,6 @@ module.exports = {
     },
     '&--error': {
       '@apply text-banano-error !important': {},
-      '.bn-checkbox__input': {
-        '@apply border-banano-error focus:ring-banano-error !important': {},
-      },
     },
     '&__input': {
       '@apply mr-2 rounded border-gray-300 focus:outline-none focus:ring-0 focus:ring-offset-0': {},
@@ -16,6 +13,9 @@ module.exports = {
       colors: {
         '@apply text-varColor-600': {},
         '@apply focus-visible:border-varColor-600 focus-visible:ring-varColor-600': {},
+      },
+      '&--error': {
+        '@apply border-banano-error focus:ring-banano-error !important': {},
       },
     },
   },
